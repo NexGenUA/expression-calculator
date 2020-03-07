c881091fbd1957bf081f06939dffd15d0c456672
NexGenUA <mr.rinus@gmail.com>
2020-03-07 17:25:27 +0200

    feat: expression calculator complete

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+	let left = 0, right = 0;
+	[...expr].forEach(el => el === '(' ? left++ : el === ')' ? right++ : '');
+	if (left != right) throw new Error('ExpressionError: Brackets must be paired');
+	if (!!~expr.indexOf('/ 0')) throw new TypeError('TypeError: Division by zero.');
+	if (!!~expr.indexOf('}') || !!~expr.indexOf('{')) throw new Error('ExpressionError: Brackets must be paired');
+	return +new Function('return ' + expr)();
 }
 
 module.exports = {
